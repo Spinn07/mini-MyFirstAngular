d5ffad361a4fb771a7a29cea3ae37d24f086139e
Henry Reed <henryreed@Henrys-MBP.home>
2016-10-22 16:12:02 -0600

    finished project

--- js/app.js
+++ js/app.js
@@ -1,2 +1,1 @@
-
-angular.module('friendsList', []);
+angular.module('friendsList',[]);
--- mainCtrl.js
+++
@@ -1,8 +0,0 @@
-angular.module('friendsList')
-.controller('mainCtrl', function($scope){
-  $scope.friends = ['Jordyn', 'Ryan', 'Chelsey'];
-  $scope.friendAdder = function(x){
-    $scope.friends.push(x);
-    $scope.adder="";
-  }
-})
