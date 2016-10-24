78cb16681c21b8df23345d7ffab015da9e9c3e15
Henry Reed <henryreed@Henrys-MacBook-Pro.local>
2016-10-24 12:21:07 -0600

    Finished project with additional search functionality

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,9 +1,9 @@
 angular.module('friendsList')
-.controller('mainCtrl', function($scope){
+.controller('mainCtrl',function($scope){
   $scope.friends = ['John', 'James','Bill'];
   $scope.addFriend = function(name){
     $scope.friends.push(name);
-    $scope.name = "";
+
   };
 
 });
